51eb10ee92d2b50f50af64676a8812b23a4c2d76
monsky31 <rajaikhsanhalomoan@gmail.com>
2022-05-26 14:02:21 +0700

    Swagger Configuration

--- src/main/java/com/latihanbuku/latihanbuku/LatihanbukuApplication.java
+++ src/main/java/com/latihanbuku/latihanbuku/LatihanbukuApplication.java
@@ -1,8 +1,16 @@
 package com.latihanbuku.latihanbuku;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -13,4 +21,26 @@ public class LatihanbukuApplication {
 		SpringApplication.run(LatihanbukuApplication.class, args);
 	}
 
+	@Bean
+	public Docket swaggerConfiguration() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.paths(PathSelectors.ant("/book/*"))
+				.apis(RequestHandlerSelectors.basePackage("com.latihanbuku.latihanbuku"))
+				.build()
+				.apiInfo(apiDetails());
+	}
+
+	private ApiInfo apiDetails() {
+		return new ApiInfo(
+				"Buku",
+				"Synrgy Batch 4 Backend Developer",
+				"1.0.0",
+				null,
+				new springfox.documentation.service.Contact("Raja Ikhsan", "https://www.linkedin.com/in/rajahalomoan/",
+						"rajaikhsanhalomoan@gmail.com"),
+				null,
+				null,
+				Collections.emptyList());
+	}
 }
